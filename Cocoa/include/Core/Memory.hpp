8feafed9000e68d209804cec94c9bc2256efa6d1
adebrabant <22966649+adebrabant@users.noreply.github.com>
2026-06-06 00:13:56 -0400

    Adding memory for smart pointers;

---
+++ Cocoa/include/Core/Memory.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <memory>
+#include <utility>
+
+namespace Cocoa
+{
+    template<typename T>
+    using Unique = std::unique_ptr<T>;
+
+    template<typename T, typename... Args>
+    Unique<T> CreateUnique(Args&&... args)
+    {
+        return std::make_unique<T>(std::forward<Args>(args)...);
+    }
+
+    template<typename T>
+    using Shared = std::shared_ptr<T>;
+
+    template<typename T, typename... Args>
+    Shared<T> CreateShared(Args&&... args)
+    {
+        return std::make_shared<T>(std::forward<Args>(args)...);
+    }
+
+    template<typename T>
+    using Weak = std::weak_ptr<T>;
+}
