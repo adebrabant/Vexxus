36105aaeab824e5aeb0ab98f7ce4620537f096a5
adebrabant <22966649+adebrabant@users.noreply.github.com>
2026-06-14 14:54:31 -0400

    Adding texture manager;

---
+++ Cocoa/include/Graphics/TextureManager.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Core/Memory.hpp"
+#include "Graphics/Texture2D.hpp"
+
+#include <cstdint>
+#include <unordered_map>
+#include <string>
+
+namespace Cocoa::Graphics
+{
+	class GraphicsDevice;
+	struct TextureSpec;
+
+	struct TextureHandle
+	{
+		uint32_t id;
+	};
+
+	class TextureManager
+	{
+	public:
+		TextureManager(GraphicsDevice& graphicsDevice);
+		~TextureManager();
+
+		TextureManager(const TextureManager&) = delete;
+		TextureManager& operator=(const TextureManager&) = delete;
+
+		TextureHandle Load(const TextureSpec& spec, const void* pixels);
+		const Texture2D& Get(TextureHandle handle) const;
+
+	private:
+		GraphicsDevice& m_graphicsDevice;
+		std::unordered_map<std::string, TextureHandle> m_handles;
+		std::unordered_map<uint32_t, Unique<Texture2D>> m_textures;
+		uint32_t m_nextId;
+	};
+}
---
+++ Cocoa/src/Graphics/TextureManager.cpp
@@ -0,0 +1,62 @@
+#include "Graphics/TextureManager.hpp"
+#include "Graphics/GraphicsDevice.hpp"
+#include "Graphics/TextureSpec.hpp"
+#include "Graphics/Texture2D.hpp"
+#include "Core/Memory.hpp"
+
+#include <string>
+#include <stdexcept>
+
+namespace Cocoa::Graphics
+{
+	TextureManager::TextureManager(GraphicsDevice& graphicsDevice) :
+		m_graphicsDevice(graphicsDevice),
+		m_handles(),
+		m_textures(),
+		m_nextId(1)
+	{
+
+	}
+
+	TextureManager::~TextureManager() = default;
+
+
+	TextureHandle TextureManager::Load(const TextureSpec& spec, const void* pixels)
+	{
+		if (auto it = m_handles.find(spec.Id); it != m_handles.end())
+		{
+			return it->second;
+		}
+
+		if (pixels == nullptr)
+		{
+			throw std::runtime_error("Texture pixels cannot be null.");
+		}
+
+		TextureHandle handle{ .id = m_nextId++ };
+		Unique<Texture2D> texture = m_graphicsDevice.CreateTexture2D(
+			spec,
+			static_cast<const unsigned char*>(pixels)
+		);
+
+		m_handles.emplace(spec.Id, handle);
+		m_textures.emplace(handle.id, std::move(texture));
+
+		return handle;
+	}
+
+	const Texture2D& TextureManager::Get(TextureHandle handle) const
+	{
+		auto it = m_textures.find(handle.id);
+
+		if (it == m_textures.end())
+		{
+			throw std::runtime_error(
+				"No Texture found with the texture handle id: " +
+				std::to_string(handle.id)
+			);
+		}
+
+		return *it->second;
+	}
+}
