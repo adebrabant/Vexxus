a9f9661e635af5b83a68884043b120ae812283ae
adebrabant <22966649+adebrabant@users.noreply.github.com>
2026-06-18 17:27:58 -0400

    Adding LoadShader tests;

--- Cocoa.Tests/src/Assets/AssetManagerTests.cpp
+++ Cocoa.Tests/src/Assets/AssetManagerTests.cpp
@@ -1,4 +1,5 @@
 #include <Assets/AssetManager.hpp>
+#include <Assets/ShaderSource.hpp>
 #include <Assets/Image.hpp>
 
 #include <gtest/gtest.h>
@@ -35,6 +36,46 @@ namespace Cocoa::Assets::Tests
 
 		const Image& result1 = sut.LoadImage("Images/test.png");
 		const Image& result2 = sut.LoadImage("Images/test2.png");
+
+		EXPECT_NE(&result1, &result2);
+	}
+
+	TEST(AssetManagerTests, LoadShader_ShouldReturnShaderSource_WhenProvidingPaths)
+	{
+		const std::string shaderId = "test-id";
+		const std::string vertexPath = "Texts/sample.txt";
+		const std::string fragmentPath = "Texts/sample2.txt";
+		const std::filesystem::path rootPath = "TestData/Resources";
+		AssetManager sut(rootPath);
+
+		const ShaderSource& result = sut.LoadShader(shaderId, vertexPath, fragmentPath);
+
+		EXPECT_NE(&result, nullptr);
+	}
+
+	TEST(AssetManagerTests, LoadShader_ShouldReturnSameShaderSource_WhenProvidingSameShaderId)
+	{
+		const std::string shaderId = "test-id";
+		const std::string vertexPath = "Texts/sample.txt";
+		const std::string fragmentPath = "Texts/sample2.txt";
+		const std::filesystem::path rootPath = "TestData/Resources";
+		AssetManager sut(rootPath);
+
+		const ShaderSource& result1 = sut.LoadShader(shaderId, vertexPath, fragmentPath);
+		const ShaderSource& result2 = sut.LoadShader(shaderId, vertexPath, fragmentPath);
+
+		EXPECT_EQ(&result1, &result2);
+	}
+
+	TEST(AssetManagerTests, LoadShader_ShouldReturnDifferentShaderSource_WhenProvidingDifferentShaderId)
+	{
+		const std::string vertexPath = "Texts/sample.txt";
+		const std::string fragmentPath = "Texts/sample2.txt";
+		const std::filesystem::path rootPath = "TestData/Resources";
+		AssetManager sut(rootPath);
+
+		const ShaderSource& result1 = sut.LoadShader("test-id", vertexPath, fragmentPath);
+		const ShaderSource& result2 = sut.LoadShader("some-id", vertexPath, fragmentPath);
 
 		EXPECT_NE(&result1, &result2);
 	}
